ad0eb1a6039ff875128aed9145db9b5eff723c13
EysiW <eystein@waade.net>
2020-09-02 13:33:59 +0200

    removed tests not created

--- src/test/com/BlackJack/DealerTest.java
+++
@@ -1,7 +0,0 @@
-package com.BlackJack;
-
-import static org.junit.Assert.*;
-
-public class DealerTest {
-    
-}
--- src/test/com/BlackJack/FinnBlackJackTest.java
+++
@@ -1,7 +0,0 @@
-package com.BlackJack;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class FinnBlackJackTest {
-
-}
